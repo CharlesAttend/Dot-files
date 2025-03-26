ae9ab6a4fd35cfaab65fed0aa021510fd79a334f
Charles Arch Linux <26749794+CharlesAttend@users.noreply.github.com>
2025-03-26 20:32:40 +0100

    hyprsunset instead of hyprshade

--- yin_yang/light-theme.sh
+++ yin_yang/light-theme.sh
@@ -2,4 +2,4 @@
 gsettings set org.gnome.desktop.interface color-scheme "prefer-light"
 sed -i "s/Mocha/Latte/" ~/.config/qt6ct/qt6ct.conf
 kitty +kitten themes --reload-in=all "Catppuccin-Latte"
-hyprshade on vibrance
+hyprctl hyprsunset identity
